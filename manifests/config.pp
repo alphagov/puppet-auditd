d1d730348f6d8b0ab0e8aff86272a7916ba27f11
Max Griffiths <maxgriff@thoughtworks.com>
2014-02-04 17:55:04 +0000

    ida-486: Use puppetlabs/concat to build audit.rules
    This change introduces the cabability to have multiple
    modules contribute to /etc/audit/audit.rules by using
    the audit::rule defined type.
    
    The original template is used as a 'begin' fragment
    and an immutable option has been introduced which
    must be the last rule, hence order=99.

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,13 +13,28 @@ class auditd::config(
     $failure_mode = 1
   }
 
-  file { '/etc/audit/audit.rules':
-    ensure  => file,
-    content => template('auditd/audit.rules.erb'),
+  $rules_file = '/etc/audit/audit.rules'
+
+  concat { $rules_file:
     mode    => '0600',
     owner   => 'root',
     group   => 'root',
   }
+
+  concat::fragment{ 'auditd_rules_begin':
+    target  => $rules_file,
+    content => template('auditd/audit.rules.begin.fragment.erb'),
+    order   => '01'
+  }
+
+  if $immutable {
+    concat::fragment{ 'auditd_rules_end':
+      target  => $rules_file,
+      content => "\n## Make the configuration immutable\n-e 2",
+      order   => '99'
+    }
+  }
+
   file { '/etc/audit/auditd.conf':
     ensure  => file,
     source  => 'puppet:///modules/auditd/etc/audit/auditd.conf',
