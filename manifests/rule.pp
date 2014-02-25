43d63718080dacf1862892fb6e18937ae3c6fded
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-02-25 10:25:36 +0000

    Fix linting in manfests/rule.pp

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,11 +1,12 @@
-define auditd::rule($content="", $order=10) {
-  if $content == "" {
+# == Define: auditd::rule
+define auditd::rule($content='', $order=10) {
+  if $content == '' {
     $body = $name
   } else {
     $body = $content
   }
 
-  concat::fragment{ "auditd_fragment_$name":
+  concat::fragment{ "auditd_fragment_${name}":
     target  => $auditd::config::rules_file,
     order   => $order,
     content => $body,
