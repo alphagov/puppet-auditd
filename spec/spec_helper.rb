09371d023e65907d655971e63e808172e893b63f
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-11-07 17:43:54 +0000

    Add base scaffolding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,10 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |c|
+
+  c.default_facts = {
+    :operatingsystem   => 'Ubuntu',
+    :lsbmajdistrelease => 14,
+  }
+
+end
