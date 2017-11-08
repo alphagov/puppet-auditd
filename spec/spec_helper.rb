0ce3b7228a2c32f0b126b0732e67a55286961389
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-11-08 17:46:33 +0000

    Add spec fact default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,11 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
 
   c.default_facts = {
+    :lsbdistcodename        => 'trusty',
+    :lsbmajdistrelease      => 14,
     :operatingsystem        => 'Ubuntu',
     :operatingsystemrelease => 'trusty',
     :osfamily               => 'Debian',
-    :lsbmajdistrelease      => 14,
   }
 
 end
